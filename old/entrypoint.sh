72c4aa25445de61cc8f561be2d7a9c0aeb592477
vmsou <viniciusmarcs@icloud.com>
2024-05-13 17:26:16 -0300

    delete: old files (dockers, configs, notebooks)

--- old/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-service ssh restart
-ssh -o "StrictHostKeyChecking no" root@localhost << EOF
-/home/hadoop/sbin/start-dfs.sh
-/home/hadoop/sbin/start-yarn.sh
-EOF
-bash
-# tail -f /dev/null  # keeps running
