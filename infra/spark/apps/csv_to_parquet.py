0e39fdf3b86194126b9891f8a86e73c42571163a
vmsou <viniciusmarcs@icloud.com>
2024-05-14 16:30:26 -0300

    feat: CSVtoParquet (better args)

--- infra/spark/apps/csv_to_parquet.py
+++ infra/spark/apps/csv_to_parquet.py
@@ -1,11 +1,12 @@
 import argparse 
+from pathlib import Path
 
 from pyspark.sql import SparkSession
 
 def parse_arguments():
 	parser = argparse.ArgumentParser(description="CSV to Parquet")
-	parser.add_argument("--source", help="Data Source URL", default="https://web.stanford.edu/class/archive/cs/cs109/cs109.1166/stuff/titanic.csv")
-	parser.add_argument("--output", help="Data Output URL")
+	parser.add_argument("-s", "--source", help="Data Source URL", default="https://web.stanford.edu/class/archive/cs/cs109/cs109.1166/stuff/titanic.csv")
+	parser.add_argument("-o", "--output", help="Data Output Folder URL", default=".")
 	return parser.parse_args()
 
 
@@ -14,9 +15,12 @@ def main():
     source: str = args.source
     output: str = args.output
 
+    file_path = Path(source)
+    output_path = (Path(output) / file_path.name).with_suffix(".parquet")
+
     spark = SparkSession.builder.appName("CSVtoParquet").getOrCreate()
-    df = spark.read.options(delimiter=',', header=True, inferSchema=True).csv(source)
-    df.write.mode("overwrite").parquet(output)
+    df = spark.read.options(delimiter=',', header=True, inferSchema=True).csv(str(file_path))
+    df.write.mode("overwrite").parquet(str(output_path))
 
 if __name__ == "__main__":
     main()
