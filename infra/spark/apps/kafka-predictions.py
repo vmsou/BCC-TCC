20163d5f7b6ffc9fd3e8ea25b09e8f802688dd93
vmsou <viniciusmarcs@icloud.com>
2024-05-16 14:37:52 -0300

    feat: Spark App - Kafka Predictions (argparse)

--- infra/spark/apps/kafka-predictions.py
+++ infra/spark/apps/kafka-predictions.py
@@ -1,39 +1,53 @@
+import argparse
 import json
+
+import pyspark.sql.functions as F
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
 from pyspark.ml.pipeline import PipelineModel
-import pyspark.sql.functions as F
 from pyspark.sql.types import StructType
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="KafkaPredictions")
+    parser.add_argument("-s", "--servers", nargs="+", help="kafka.bootstrap.servers (i.e. <ip1>:<host1> <ip2>:<host2> ... <ipN>:<hostN>)", default=["kafka:9092"])
+    parser.add_argument("-t", "--topic", help="Kafka Topic (i.e. topic1)", default="NetV2")
+    parser.add_argument("-m", "--model", help="Path to Model", default="models/DecisionTreeClassifier_NF-UNSW-NB15-v2_MODEL")
+    parser.add_argument("--schema", help="Path to Schema JSON", default="schemas/NetV2_schema.json")
+    return parser.parse_args()
 
 
 def spark_schema_from_json(spark: SparkSession, path: str) -> StructType:
     schema_json = spark.read.text(path).first()[0]
     return StructType.fromJson(json.loads(schema_json))
 
-def schema_to_DDL(schema: StructType) -> str:
-    ddl_fields = []
-    for field in schema.fields:
-        ddl_field = f"{field.name} {field.dataType.simpleString()}"
-        ddl_fields.append(ddl_field)
-    return ", ".join(ddl_fields)
-
 
 def main() -> None:
+    args = parse_arguments()
+    servers: str = ",".join(args.servers)
+    topic: str = args.topic
+    model_path: str = args.model
+    schema_path: str = args.schema
+
+    #.config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0") \
     spark: SparkSession = SparkSession.builder \
                         .appName("NetV2Predictions") \
                         .config("spark.cores.max", '3') \
                         .getOrCreate()
+    
     sc: SparkContext = spark.sparkContext
     sc.setLogLevel("WARN")
 
-    model = PipelineModel.load("./models/DecisionTreeClassifier_NF-UNSW-NB15-v2_MODEL")
-    schema = spark_schema_from_json(spark, "./NetV2_schema.json")
+    model = PipelineModel.load(model_path)
+    # sc.broadcast(model)
+
+    schema = spark_schema_from_json(spark, schema_path)
 
     df = spark \
         .readStream \
         .format("kafka") \
-        .option("kafka.bootstrap.servers", "kafka:9092") \
-        .option("subscribe", "netV2") \
+        .option("kafka.bootstrap.servers", servers) \
+        .option("subscribe", topic) \
         .option("startingOffsets", "earliest") \
         .load()
 
@@ -42,15 +56,21 @@ def main() -> None:
             .select(F.from_csv("value", schema.simpleString()).alias("features")) \
             .select("features.*")
 
+    # features = df \
+    #     .selectExpr("CAST(value AS STRING) AS value") \
+    #     .selectExpr(f"from_csv(value, '{schema.simpleString()}') AS features") \
+    #     .select("features.*")
+
     predictions = model.transform(features).select("features", "prediction", "probability")
 
     query = predictions.writeStream \
+            .queryName("NetV2 Predictions Writer") \
             .format("console") \
-            .outputMode("update") \
+            .outputMode("append") \
             .start()
     query.awaitTermination()
 
-    sc.close()
+    spark.close()
 
 
 if __name__ == "__main__":
