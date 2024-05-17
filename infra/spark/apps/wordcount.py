add3d758c50300f320bcced22c90a3dd93c2de87
vmsou <viniciusmarcs@icloud.com>
2024-05-17 15:14:33 -0300

    update: Spark Apps - Word Count (main function)

--- infra/spark/apps/wordcount.py
+++ infra/spark/apps/wordcount.py
@@ -1,34 +1,42 @@
+from pyspark import SparkContext
 from pyspark.sql import SparkSession
-import pyspark.sql.functions as funcs
-
-#.config("spark.streaming.stopGracefullyOnShutdown", True) \  # needs hadoop create /tmp
-spark: SparkSession = SparkSession.builder \
-                    .appName("WordCountKafkaTest") \
-                    .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.4.3") \
-                    .getOrCreate()
-
-spark.sparkContext.setLogLevel("WARN")
-
-df = spark \
-    .readStream \
-    .format("kafka") \
-    .option("kafka.bootstrap.servers", "kafka-server:9092") \
-    .option("subscribe", "words") \
-    .option("startingOffsets", "earliest") \
-    .load()
-
-words = df.select(
-    funcs.explode(
-        funcs.split(df['value'], " ")
-    ).alias("word")
-)
-
-words_count = words.groupBy("word").count()
+import pyspark.sql.functions as F
 
 
-query = words_count.writeStream \
-        .format("console") \
-        .outputMode("complete") \
-        .trigger(processingTime='10 seconds') \
-        .start() \
-        .awaitTermination()
+def main() -> None:
+    spark: SparkSession = SparkSession.builder \
+                        .appName("WordCountKafkaTest") \
+                        .config("spark.cores.max", '3') \
+                        .getOrCreate()
+
+    sc: SparkContext = spark.sparkContext
+    sc.setLogLevel("WARN")
+    # sc.setCheckpointDir("hdfs:///user/spark/tmp/")
+
+    df = spark \
+        .readStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", "kafka:9092") \
+        .option("subscribe", "words") \
+        .option("startingOffsets", "earliest") \
+        .load()
+
+    words = df.select(
+        F.explode(
+            F.split(F.col("value"), " ")
+        ).alias("word")
+    )
+
+    words_count = words.groupBy("word").count()
+
+    query = words_count.writeStream \
+            .format("console") \
+            .outputMode("complete") \
+            .start()
+    query.awaitTermination()
+
+    spark.close()
+
+
+if __name__ == "__main__":
+    main()
